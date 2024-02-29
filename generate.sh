f5e57ce80ca01adeaddde5a8123c84632cd8ca54
Andrew Martin <a.a.d.martin@gmail.com>
2024-02-29 15:14:17 +1100

    updated generator

--- generate.sh
+++ generate.sh
@@ -21,9 +21,9 @@ cat << EOF > "${test_filename}"
 
 package typeconv
 
-import (
-    "testing"
-)
+//import (
+//    "testing"
+//)
 
 EOF
 
@@ -31,35 +31,37 @@ EOF
     for input in $list; do
       if [[ "$input" != "$output" ]]; then
 
+        input_lower=$(echo $input | awk '{print tolower($0)}')
+
 cat << EOF >> "${filename}"
-// ${input}To${output} as the name implies takes a ${input} and converts it into a ${output}
-// func ${input}To${output}(input $input) (${output}, error) {
-//
+// ${input}To${output} as the name implies takes a ${input_lower} and converts it into a ${output_lower}
+// func ${input}To${output}(input $input_lower) (${output_lower}, error) {
+
 //   NOT IMPLEMENTED YET
-//
+
 //}
 
 
 EOF
 
 cat << EOF >> "${test_filename}"
+//func Test${input}To${output}(t *testing.T) {
 
-func Test${input}To${output}(t *testing.T) {
+//    input := ${input_lower}(1)
+//    expected := ${output_lower}(1)
 
-    input := ${input}(1)
-    expected := ${output}(1)
+//    result, err := ${input}To${output}(input)
 
-    result, err := ${input}To${output}(input)
+//    if err != nil {
+//        t.Errorf("Error %v", err)
+//    }
 
-    if err != nil {
-        t.Errorf("Error %v", err)
-    }
+//    if result != expected {
+//        t.Errorf("Result was Incorrect, got: %s, wanted: %s.", result, expected)
+//    }
 
-    if result != expected {
-        t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
-    }
+//}
 
-}
 
 EOF
 
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/adamar/typeconv
 
-go 1.21.3
+go 1.23
